5cc22cc6623f8d80a31d30cdbc70b875c3e8232c
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-03 22:54:24 +0200

    Script che stampa in console la nuova lista con input utente

--- script.js
+++ script.js
@@ -18,9 +18,7 @@ var cognomi = [
   "Dybala"
 ];
 
-var nuovoCognome;
-
-// console.log(cognomi[9]);
+var nuovoCognome, contatore, lengthArray;
 
 // Chiedo un nuovo Cognome all'utente
 nuovoCognome = prompt('inserisci il prossimo acquisto magico della Juventus f.c');
@@ -29,3 +27,17 @@ console.log('Dato inserito da utente: ' + nuovoCognome);
 //Inserisco input nell'Array
 cognomi.push(nuovoCognome);
 console.log(cognomi);
+
+// Stampo l'elenco dei Cognomi
+lengthArray = cognomi.length;
+console.log(lengthArray);
+
+//Chiudo il contatore in una variabile
+contatore = 0;
+
+// Stampo in console la nuova lista aggiornata
+while (contatore < lengthArray) {
+  console.log([contatore] + " " + cognomi[contatore]);
+
+  contatore++;
+}
