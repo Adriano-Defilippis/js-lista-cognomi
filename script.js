8243c8abeb1a19ed8352219212880286ec938544
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-03 22:23:00 +0200

    Valorizzato Array di Cognomi con 11 elementi

---
+++ script.js
@@ -0,0 +1,21 @@
+//chiedi all'utente un Cognome
+// Inseriscilo in un Array con altri Cognomi
+// Stampa l'elenco aggiornato ed in ordine alfabetico
+// Scrivere l aposizione nella lista del nuovo utente inserito
+
+// Valorizzo un Array con Gia dei Cognomi
+var cognomi = [
+  "Buffon",
+  "Barzagli",
+  "Chiellini",
+  "Bonucci",
+  "Alex sandro",
+  "Pjanic",
+  "Matuidi",
+  "Bernardeschi",
+  "Mandzuckic",
+  "Ronaldo",
+  "Dybala"
+];
+
+console.log(cognomi[9]);
